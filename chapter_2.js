ebb8afdc90e4c38892431ace696c7e8d38a5d256
zbf84 <zonaid84@gmail.com>
2026-05-20 10:54:45 +0600

    Add files via upload

---
+++ chapter_2.js
@@ -0,0 +1,14 @@
+//3
+const totalMoney = 60;
+const expensePerEvent = 15;
+const occur = totalMoney / expensePerEvent;
+const remaining = totalMoney % expensePerEvent;
+// console.log(occur);
+// console.log(remaining);
+
+//4
+let string1 = "Hello" ;
+let string2="World"
+console.log(string1 + " "+string2);
+
+
