7b8c1463ddea36f0cb8d8487f70d10d1fbad320c
zbf84 <zonaid84@gmail.com>
2026-05-20 16:09:18 +0600

    Update chapter_2.js

--- chapter_2.js
+++ chapter_2.js
@@ -11,4 +11,38 @@ let string1 = "Hello" ;
 let string2="World"
 console.log(string1 + " "+string2);
 
+//5
+let rice = 153;
+let lentil=261;
+let total = rice+lentil;
+// console.log(total);
 
+//6
+const total_Money = 500;
+const expense_PerEvent = 75;
+const  occured = parseInt(total_Money / expense_PerEvent);
+const remainings = total_Money % expense_PerEvent;
+// console.log(occured);
+// console.log(remainings);
+
+//7
+const result = 8 / 3;
+const remaininG = 8 % 3;
+// console.log(result);
+// console.log(remaininG);
+
+//8
+const remaining_8 = 50 % 9;
+// console.log(remaining_8);
+
+//9
+const first = "Bangla";
+const second = "desh";
+// console.log(first+second);
+
+//10
+const riceTotal = 480;
+const dailyConsumption = 4;
+const totalDayInMonth =30;
+const totalMonths = (480/4)/30;
+console.log(totalMonths);
